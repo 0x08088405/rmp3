8b2bdd285292c1287e9df66d69d0f97cbc99fbf7
viri <git@stuff.moe>
2020-01-18 16:48:34 -0600

    ffi: rename include.h to bindgen.h

--- ffi/generate.sh
+++ ffi/generate.sh
@@ -1,3 +1,3 @@
 # easy script for regenerating bindings
 git submodule update --init --recursive
-bindgen src/include.h --output src/bindings.rs -- -Iminimp3
+bindgen src/bindgen.h --output src/bindings.rs -- -Iminimp3
