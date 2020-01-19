aed4361bb01423e3a344eed54388f6dcf98f5d17
viri <git@stuff.moe>
2020-01-18 21:46:20 -0600

    clarify what ffi/bindgen.sh is for

--- ffi/bindgen.sh
+++ ffi/bindgen.sh
@@ -1,4 +1,4 @@
-# easy script for regenerating bindings
-# run from repository root btw, also install `cargo install bindgen` cli if you haven't
+# easy script for regenerating bindings (mainly for myself, NOT part of the build process)
+# run from repository root, also install `cargo install bindgen` cli if you haven't
 git submodule update --init --recursive
 bindgen ffi/bindgen.h --output src/bindings.rs -- -Iffi/minimp3
