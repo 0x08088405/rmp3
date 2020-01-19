8432421f320fc8b5809cfa10acd70aa46dece99a
viri <git@stuff.moe>
2020-01-19 04:07:47 -0600

    add only-mp3 feature (MINIMP3_ONLY_MP3)

--- build.rs
+++ build.rs
@@ -1,8 +1,14 @@
 fn main() {
     println!("cargo:rerun-if-changed=ffi/minimp3.c");
-    cc::Build::new()
+    let mut build = cc::Build::new();
+
+    build
         .include("ffi/minimp3")
-        .define("MINIMP3_IMPLEMENTATION", None)
-        .file("ffi/minimp3.c")
-        .compile("minimp3");
+        .define("MINIMP3_IMPLEMENTATION", None);
+
+    if cfg!(feature = "only-mp3") {
+        build.define("MINIMP3_ONLY_MP3", None);
+    }
+
+    build.file("ffi/minimp3.c").compile("minimp3");
 }
