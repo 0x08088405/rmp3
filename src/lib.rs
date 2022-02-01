11c59c21b312a910f19ef6d4e47d48fbba7a099b
viri <hi@viri.moe>
2022-02-01 02:41:26 -0600

    lib: improve debug fmt, document crate

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,4 @@
-//! This is a cool library!
-
-// TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO
-//#![deny(missing_docs)]
+#![warn(missing_docs)]
 
 #![no_std]
 
@@ -14,11 +11,16 @@ use chlorine::c_int;
 /// Maximum number of samples per frame.
 pub const MAX_SAMPLES: usize = 1152 * 2;
 
+/// Describes a frame, which may contain audio samples.
 pub enum Frame<'src, 'pcm> {
+    /// A frame containing PCM data.
     Audio(Audio<'src, 'pcm>),
+
+    /// A frame containing miscellaneous data.
     Other(&'src [u8]),
 }
 
+#[derive(Clone)]
 pub struct Audio<'src, 'pcm> {
     bitrate: u16,
     channels: u8,
@@ -37,6 +39,8 @@ unsafe impl<'src, 'pcm> Sync for Audio<'src, 'pcm> {}
 
 impl<'src, 'pcm> Audio<'src, 'pcm> {
     /// Gets the bitrate of this frame in kb/s.
+    ///
+    /// Interval: x ∈ [8, 448]
     #[inline]
     pub fn bitrate(&self) -> u16 {
         // TODO check what happens with the reserved bitrates
@@ -56,7 +60,7 @@ impl<'src, 'pcm> Audio<'src, 'pcm> {
         self.mpeg_layer as u8
     }
 
-    /// Gets the number of samples in this frame.
+    /// Gets the number of samples in this frame per [channel](Self::channels).
     #[inline]
     pub fn sample_count(&self) -> NonZeroU16 {
         self.sample_count
@@ -71,8 +75,11 @@ impl<'src, 'pcm> Audio<'src, 'pcm> {
 
     /// Gets the slice of decoded samples.
     ///
-    /// If [`inspect`](Decoder::inspect) was used,
-    /// this slice will be empty, as nothing was decoded.
+    /// Samples are interleaved, so this has the length of
+    /// [`channels`](Self::channels) * [`sample_count`](Self::sample_count),
+    /// to a maximum of [`MAX_SAMPLES`](crate::MAX_SAMPLES).
+    ///
+    /// If the samples weren't decoded, this is an empty slice.
     #[inline]
     pub fn samples(&self) -> &'pcm [f32] {
         if let Some(buf) = self.pcm {
@@ -82,6 +89,9 @@ impl<'src, 'pcm> Audio<'src, 'pcm> {
         }
     }
 
+    /// Gets the slice of the source which contains the entire frame.
+    ///
+    /// Leading garbage is omitted from the slice.
     #[inline]
     pub fn source(&self) -> &'src [u8] {
         self.src
@@ -92,7 +102,7 @@ impl fmt::Debug for Frame<'_, '_> {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         match self {
             Self::Audio(audio) => f.debug_tuple("Audio").field(audio).finish(),
-            Self::Other(_) => f.debug_tuple("Other").field(&"&[...]").finish(),
+            Self::Other(_) => f.debug_tuple("Other").field(&format_args!("&[...]")).finish(),
         }
     }
 }
@@ -106,19 +116,72 @@ impl fmt::Debug for Audio<'_, '_> {
             .field("sample_count", &self.sample_count)
             .field("sample_rate", &self.sample_rate)
             .field("samples", {
-                if self.pcm.is_some() {
-                    &"&[...]"
+                &if self.pcm.is_some() {
+                    format_args!("&[...]")
                 } else {
-                    &"&[not decoded]"
+                    format_args!("&[not decoded]")
                 }
             })
             .finish()
     }
 }
 
+/// Primitive stateless decoder for parsing and/or decoding MPEG Audio.
+///
+/// # Examples
+///
+/// Simple example decoding frames into a big `Vec` of all the samples.
+///
+/// ```no_run
+/// # fn main() {
+/// use empy::{Decoder, Frame};
+///
+/// let mut data: &[u8] = &[/* your file here */];
+///
+/// let mut decoder = Decoder::new();
+/// let mut buffer = [0.0; empy::MAX_SAMPLES];
+/// let mut pcm: Vec<f32> = Vec::with_capacity(1024 * 1024 * 32);
+///
+/// while let Some((frame, bytes_consumed)) = decoder.decode(data, Some(&mut buffer)) {
+///     match frame {
+///         Frame::Audio(audio) => {
+///             // note that you'd want to keep track of bitrate, channels, sample_rate
+///             // they can differ between adjacent frames (especially bitrate for VBR)
+///             pcm.extend_from_slice(audio.samples());
+///         },
+///         Frame::Other(_) => (/* don't care */),
+///     }
+///     data = &data[bytes_consumed..];
+/// }
+/// # }
+/// ```
+///
+/// You don't need to decode the samples if it's not necessary.
+/// Example computing length in minutes and seconds:
+///
+/// ```no_run
+/// # fn main() {
+/// use empy::{Decoder, Frame};
+///
+/// let mut data: &[u8] = &[/* your file here */];
+/// let mut decoder = Decoder::new();
+/// let mut length = 0.0f64;
+///
+/// while let Some((frame, bytes_consumed)) = decoder.decode(data, None) {
+///     if let Frame::Audio(audio) = frame {
+///         // note here that sample_count is *per channel* so it works out
+///         length += f64::from(audio.sample_count().get()) / f64::from(audio.sample_rate().get());
+///     }
+///     data = &data[bytes_consumed..];
+/// }
+///
+/// println!("Length: {:.0}m{:.0}s", length / 60.0, length % 60.0);
+/// # }
+/// ```
 pub struct Decoder(MaybeUninit<ffi::mp3dec_t>);
 
 impl Decoder {
+    /// Initialises a new [`Decoder`].
     #[inline]
     pub fn new() -> Self {
         let mut state = MaybeUninit::uninit();
@@ -126,24 +189,15 @@ impl Decoder {
         Self(state)
     }
 
-    #[inline]
+    /// Tries to find and decode a frame in `src`.
+    ///
+    /// Decoding the samples will be skipped if `dest` is [`None`](Option::None).
+    ///
+    /// If there's some garbage present before the framesync, it will be skipped.
+    ///
+    /// On success, returns information about the [`Frame`],
+    /// and how many bytes it read total (including garbage, if any).
     pub fn decode<'src, 'pcm>(
-        &mut self,
-        src: &'src [u8],
-        dest: &'pcm mut [f32; MAX_SAMPLES],
-    ) -> Option<(Frame<'src, 'pcm>, usize)> {
-        self.process(src, Some(dest))
-    }
-
-    #[inline]
-    pub fn inspect<'src>(
-        &mut self,
-        src: &'src [u8],
-    ) -> Option<(Frame<'src, 'static>, usize)> {
-        self.process(src, None)
-    }
-
-    fn process<'src, 'pcm>(
         &mut self,
         src: &'src [u8],
         dest: Option<&'pcm mut [f32; MAX_SAMPLES]>,
