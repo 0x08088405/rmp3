220872910e4af6ae5090aa86f9f1802ae00f670e
viri <hi@viri.moe>
2022-02-03 01:54:41 -0600

    cargo: bump to alpha2

--- build.rs
+++ build.rs
@@ -1,15 +1,17 @@
 fn main() {
     println!("cargo:rerun-if-changed=ffi/minimp3.c");
+
     let mut build = cc::Build::new();
 
     build.include("ffi/minimp3");
     build.define("MINIMP3_FLOAT_OUTPUT", None);
 
-    if cfg!(not(feature = "simd")) {
-        build.define("MINIMP3_NO_SIMD", None);
-    }
     if cfg!(not(feature = "mp1-mp2")) {
         build.define("MINIMP3_ONLY_MP3", None);
+    }
+
+    if cfg!(not(feature = "simd")) {
+        build.define("MINIMP3_NO_SIMD", None);
     }
 
     build
